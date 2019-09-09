6c3c1a3431c99eaf500f144e31a234e98d110e3b
Edgar V <54725200+evm100@users.noreply.github.com>
2019-09-09 13:06:50 -0700

    Update RandomLogic.java

--- MidtermTestP4/RandomLogic.java
+++ MidtermTestP4/RandomLogic.java
@@ -2,9 +2,4 @@ public class RandomLogic {
     public void askQuestion() {
         // Add cod
         df
-            sdf
-            sd
-            fds
-            fdse that asks the question and retrives an answer here
-    }
-}
+      asdasdasdasdasdasdasd
