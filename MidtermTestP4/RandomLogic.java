33a77711b3b1fc982b180358c77fcc4410103564
Edgar V <54725200+evm100@users.noreply.github.com>
2019-09-19 13:04:50 -0700

    Update RandomLogic.java

--- MidtermTestP4/RandomLogic.java
+++ MidtermTestP4/RandomLogic.java
@@ -1,4 +1,13 @@
 public class RandomLogic {
     public void askQuestion() {
+    /*
+    Create the questions    
+    Based off the arguments in the question, calculate the answers
+    Calculate answer
+    Calculate incorrect answers
+    Display possible answers
+    Compare user input to see if it is correct answer
+    */
+    
     }
 }
